51bf73c253ff210f3863413407f42aa2c5b0ae65
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-02-11 15:04:34 +0000

    Outline of the Students Database Application.

---
+++ src/studentsdatabseapp/Student.java
@@ -0,0 +1,31 @@
+package studentsdatabseapp;
+
+public class Student {
+
+    private String name;
+
+    private short year;
+
+    private int id;
+
+    private byte gradeLevel;
+
+    private int balance;
+
+    private byte[] courses;
+
+    private final static short COURSE_FEE = 600;
+
+    // Constructor: Add new students (prompt for name and year)
+
+    // Generate id
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+
+}
---
+++ src/studentsdatabseapp/StudentsDatabaseApp.java
@@ -0,0 +1,13 @@
+package studentsdatabseapp;
+
+public class StudentsDatabaseApp {
+
+    public static void main(String[] args) {
+
+        // Ask how many users should be added
+
+        // Create n number of new students
+
+    }
+
+}
