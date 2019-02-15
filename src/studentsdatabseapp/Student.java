ba17a33b6d70891b6647f6a31d0bf2defcd26c98
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-02-15 17:41:01 +0000

    Basic functionality of the students database app done. (closes #2)

--- src/studentsdatabseapp/Student.java
+++ src/studentsdatabseapp/Student.java
@@ -1,5 +1,10 @@
 package studentsdatabseapp;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
@@ -8,24 +13,111 @@ public class Student {
 
     private short gradeYear;
 
-    private int studentID;
+    private int studentId;
 
-    private String courses;
+    private String[] courses = {"History 101", "Mathematics 101", "English 101", "Chemistry 101", "Computer Science 101"};
 
-    private int tuitionBalance;
+    private List<Integer> enrolledCourses = new ArrayList<>();
 
-    private final static short COST_OF_COURSE = 600;
+    private int tuitionBalance = 0;
 
-    // Constructor: Add new students (prompt for name and year)
+    private static short COST_OF_EACH_COURSE = 600;
+
+    private static int id = 1000;
+
+    // Constructor: Add new student (prompt for name and year)
+    public Student() {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter student's first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student's last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("Enter student's class level: " +
+                         "\n(1) Freshman" +
+                         "\n(2) Sophmore" +
+                         "\n(3) Junior"+
+                         "\n(4) Senior" +
+                         "\n: ");
+
+        this.gradeYear = in.nextShort();
+        this.setStudentId();
+        this.enrollInCourses();
+
+    }
 
     // Generate id
+    private void setStudentId() {
+        id++;
+        // Grade Level + id
+        this.studentId = gradeYear * 10000 + id;
+    }
 
     // Enroll in courses
+    private void enrollInCourses() {
+
+        System.out.println("Enter course to enroll: ");
+        for (int i = 0; i < this.courses.length; i++) {
+            System.out.println("(" + i + ") "  + this.courses[i]);
+        }
+        System.out.println("(q) Move on");
+        System.out.print(": ");
+
+        Scanner in = new Scanner(System.in);
+        int courseId;
+        String option;
+
+        while (!(option = in.nextLine()).equals("q")) {
+            courseId = Integer.valueOf(option);
+            this.enrolledCourses.add(courseId);
+            this.tuitionBalance += COST_OF_EACH_COURSE;
+            System.out.println("The student's tuition balance now is: $" + this.getTuitionBalance());
+            System.out.print("Enter next course to enroll or (q) to quit: ");
+        }
+    }
 
     // View balance
+    public int getTuitionBalance() {
+        return tuitionBalance;
+    }
 
     // Pay tuition
+    public void payTuition() {
+
+        short amount;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter amount to be paid: $");
+        amount = in.nextShort();
+        this.tuitionBalance -= amount;
+
+        System.out.println("The new tuition balance is: $" + this.getTuitionBalance());
+    }
 
     // Show status
+    public String toString() {
+
+        String listEnrolledCourses = "";
+        int counter = 1;
+
+        for (int courseId : this.enrolledCourses) {
+            listEnrolledCourses += this.courses[courseId];
+            // Add a space only if it's not the last element
+            if(counter < this.enrolledCourses.size()) {
+                listEnrolledCourses += ", ";
+            }
+            counter++;
+        }
+
+        return "Student ID: " + studentId +
+                "\nFirst name: " + firstName +
+                "\nLast name: " + lastName +
+                "\nEnrolled courses: " + listEnrolledCourses +
+                "\nTuition balance: $" + tuitionBalance;
+    }
+
 
 }
--- src/studentsdatabseapp/StudentsDatabaseApp.java
+++ src/studentsdatabseapp/StudentsDatabaseApp.java
@@ -1,13 +1,56 @@
 package studentsdatabseapp;
 
+import java.util.Scanner;
+
 public class StudentsDatabaseApp {
 
     public static void main(String[] args) {
 
-        // Ask how many users should be added
+        Student[] studentsToEnroll = null;
 
-        // Create n number of new students
+        String startMenu =  "Choose an operation:" +
+                            "\n(1) Enroll students" +
+                            "\n(2) View status" +
+                            "\n(q) Quit" +
+                            "\n: ";
 
+        System.out.print(startMenu);
+        Scanner in = new Scanner(System.in);
+
+        String option;
+        int numberOfStudents = 0;
+
+        while (!(option = in.nextLine()).equals("q")) {
+            if (option.equals("1")) {
+                // Ask how many users should be added
+                System.out.print("Enter number of students to enroll: ");
+
+                // Create n number of new students
+                numberOfStudents = in.nextInt();
+                studentsToEnroll = new Student[numberOfStudents];
+
+                for (int i = 0; i < numberOfStudents; i++) {
+                    studentsToEnroll[i] = new Student();
+                    studentsToEnroll[i].payTuition();
+                }
+
+                in.nextLine();
+
+            } else if (option.equals("2")) {
+
+                if (studentsToEnroll != null) {
+                    for (int i = 0; i < numberOfStudents; i++) {
+                        System.out.println(studentsToEnroll[i].toString());
+                    }
+                } else {
+                    System.out.println("There are no students currently enrolled.");
+                }
+
+            } else {
+                System.out.println("\nPlease choose one of the options bellow.");
+            }
+            System.out.print(startMenu);
+        }
+        System.out.println("Good bye!");
     }
-
 }
