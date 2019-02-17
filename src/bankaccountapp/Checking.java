f987b9ae7ed309338958c9285d6d594ace89aadc
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-02-17 17:18:03 +0000

    Bank Account App fully implemented. (closes #3)

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -3,14 +3,34 @@ package bankaccountapp;
 public class Checking extends Account {
 
     // List properties specific to a checking account
-    int debitCardNumber;
-    int debitCardPIN;
+    private int debitCardNumber;
+    private int debitCardPIN;
 
     // Constructor to initialize checking account properties
     public Checking(String name, String sSN, double initialDeposit) {
         super(name, sSN, initialDeposit);
-        System.out.println("NEW CHECKING ACCOUNT");
+        accountNumber = "2" + accountNumber;
+        setDebitCard();
+    }
+
+    @Override
+    public void setRate() {
+        rate = getBaseRate() * .15;
     }
 
     // List any methods specific to the checking account
+    private void setDebitCard() {
+        debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+        debitCardPIN = (int) (Math.random() * Math.pow(10, 4));
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println(
+                " Your checking account features" +
+                "\n Debit Card Number: " + debitCardNumber +
+                "\n Debit Card PIN: " + debitCardPIN
+        );
+    }
 }
--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -3,15 +3,35 @@ package bankaccountapp;
 public class Savings extends Account {
 
     // List properties specific to a savings account
-    int safetyDepositBoxID;
-    int safetyDepositBoxKey;
+    private int safetyDepositBoxID;
+    private int safetyDepositBoxKey;
 
     // Constructor to initialize savings account properties
     public Savings(String name, String sSN, double initialDeposit) {
         super(name, sSN, initialDeposit);
-        System.out.println("NEW SAVINGS ACCOUNT");
+        accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
+    }
+
+    @Override
+    public void setRate() {
+        rate = getBaseRate() - .25;
     }
 
     // List any methods specific to the savings account
+    private void setSafetyDepositBox() {
+        safetyDepositBoxID = (int)  (Math.random() * Math.pow(10, 3));
+        safetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println(
+                " Your savings account features" +
+                "\n Safety Deposit Box ID: " + safetyDepositBoxID +
+                "\n Safety Deposit Box Key: " + safetyDepositBoxKey
+        );
+    }
 
 }
